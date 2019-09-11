a61d588212a428fe94e5e4e08a3389ac13c38604
aaron122995 <54408915+aaron122995@users.noreply.github.com>
2019-09-11 13:50:28 +0800

    Create index.js

---
+++ index.js
@@ -0,0 +1,2 @@
+alert("This is a reponsive website built up by HTML + CSS + Bootstrap. It demostrates an imaginary start up app product called TinDog
+        which is similar to tinder version for dogs.");
